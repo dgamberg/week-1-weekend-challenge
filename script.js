a524b714f1622b7c946480d41c2f5602f42d7d14
Dave Gamberg <dgamberg@gmail.com>
2015-10-18 20:34:10 -0500

    adding all javascript and css

---
+++ script.js
@@ -0,0 +1,100 @@
+var employeeArray = [];
+
+$(document).ready(function(){
+    //hide output area on load
+    $('#totalOuputArea').hide();
+    $('#top-headers').hide();
+
+    //initialize counter for each row to 0
+    var intCounter = 0;
+
+    //when form is submitted
+    $("#employeeInfo").submit(function(event){
+        event.preventDefault();
+        
+        //set up blank object
+        var values = {};
+
+        //increment counter
+        intCounter++;
+
+        //add each item to an object
+        $.each($("#employeeInfo").serializeArray(), function(i, field){
+            values[field.name] = field.value;
+        });
+
+        //clear form on submit
+        $("#employeeInfo").find("input[type=text]").val("");
+        $("#employeeInfo").find("input[type=number]").val("");
+
+        //add items to the array
+        employeeArray.push(values);
+        
+        //add items from line 22 to the DOM
+        appendDom(values, intCounter);
+
+        var outputForPage = runTotal(employeeArray);
+
+        $('#totalMonthlySalaries').text(outputForPage);
+        $('#totalOuputArea').fadeIn();
+        $('#top-headers').fadeIn();
+
+    });
+
+    //delete click event
+    $('#employeeContainer').on('click', '.deleteRowButton', function(){
+             //remove item from DOM   
+            $(this).parent().remove();
+            
+            //figure out which item is being deleted
+            var itemToDelete = $(this).find('.counter').data('counter').text();
+            
+            //remove item from the array
+            removeItem(itemToDelete,employeeArray);
+
+            //put results of run toatal in var and display   
+            var output = runTotal(employeeArray);
+            $('#totalMonthlySalaries').text(output);   
+    });
+});
+
+
+function appendDom(employee, i){
+    //add each row to the DOM when submitted
+    $("#employeeContainer").append("<div class='employee'></div>");
+
+    var $el = $("#employeeContainer").children().last();
+    
+    $el.append("<p class='counter' data-counter=' " + i +"  '></p>");
+    $el.append("<p class='employeeName'>" + employee.employeeName + "</p>");
+    $el.append("<p class='employeeNumber'>" + employee.employeeNumber + "</p>");
+    $el.append("<p class='employeeTitle'>" + employee.employeeTitle + "</p>");
+
+    var salaryOutput = parseInt(employee.employeeSalary).toLocaleString('en');
+
+    $el.append("<p class='employeeSalary'>$" + salaryOutput + "</p>");
+    
+    //delete button version
+    $el.append("<button class='deleteRowButton btn btn-danger'>delete</button>");
+
+}
+
+function runTotal(employeeArray){
+    //initialize return variable
+    var totalToReturn = 0; 
+    for (var i = 0; i < employeeArray.length; i++) {
+        // get employee salary from every row, total, put in totalToReturn
+        var inputRow = parseInt(employeeArray[i].employeeSalary/12);
+        totalToReturn = totalToReturn + inputRow;
+    }
+    return totalToReturn.toLocaleString('en');
+}
+
+//give index and an array and it will be removed
+//used with delete button
+function removeItem(index, array){
+    array.splice(index,1);
+}
+
+
+
